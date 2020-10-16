597032377661c9f07e815f92f1c97c0837ac3842
surendra singh <surendrasinghsunny473@gmail.com>
2020-10-16 12:02:25 +0530

    responsive navigation bar

---
+++ js5.js
@@ -0,0 +1,17 @@
+function showMenu() {
+    var a=document.getElementById("bar");   
+    var b=document.getElementById("times");   
+    var c=document.getElementById("sur");
+    a.style.display = "none";   
+    b.style.display = "block";   
+    c.style.display = "block";   
+
+}
+function closeMenu() {
+    var a=document.getElementById("bar");   
+    var b=document.getElementById("times");   
+    var c=document.getElementById("sur");
+    a.style.display="block";   
+    b.style.display="none";   
+    c.style.display="none";  
+}
